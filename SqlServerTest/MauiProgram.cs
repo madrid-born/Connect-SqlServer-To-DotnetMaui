1f07aa1b9ce242ac08284dcc81e124f9338e0eb8
Alireza Noori <98411848+madrid-born@users.noreply.github.com>
2024-05-01 15:38:29 +0330

    Update MauiProgram.cs

--- SqlServerTest/MauiProgram.cs
+++ SqlServerTest/MauiProgram.cs
@@ -15,15 +15,6 @@ namespace SqlServerTest ;
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-            // builder.Services.AddDbContext<UserDbContext>(options=>options.UseSqlServer(
-            //     "Server=win2016-770ir.hostnegar.com\\MSSQLSERVER2022;Database=RaterDataBase;User ID=alireza;Password=15iX#6to0;TrustServerCertificate=True"
-            //     ));
-
-            // var dbContextOptions = new DbContextOptionsBuilder<UserDbContext>()
-            //     .UseSqlServer("Server=win2016-770ir.hostnegar.com\\MSSQLSERVER2022;Database=RaterDataBase;User ID=alireza;Password=15iX#6to0;TrustServerCertificate=True")
-            //     .Options;
-
-            // builder.Services.AddSingleton(new UserDbContext(dbContextOptions));
             builder.Services.AddSingleton<UserDbContext>();
             builder.Services.AddTransient<MainPage>();
 
